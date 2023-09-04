0e268a177f1be0596b8f1536021daf1f8272f206
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-09-04 17:54:44 +0200

    log in with no styling

--- src/components/pages/frontpage/parts/eventsdisplay.jsx
+++ src/components/pages/frontpage/parts/eventsdisplay.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 
 const EventDisplay = () => {
@@ -31,6 +32,9 @@ const EventDisplay = () => {
           <img src={`http://localhost:4000/Assets/Images/events/small/${data.image}`} alt="img" />
           <p>{data.startdate}-{data.stopdate}</p>
           <h2>{data.title}</h2>
+          <Link >
+          <button>læs mere</button>
+          </Link>
         </figure>
       ))}
     </div>
