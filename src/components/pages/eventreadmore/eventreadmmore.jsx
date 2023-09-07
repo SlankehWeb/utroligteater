e9c35b6adb0d90f2d8786b94a3acfde52a10608e
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-09-07 12:12:39 +0200

    fetch for event details

--- src/components/pages/eventreadmore/eventreadmmore.jsx
+++ src/components/pages/eventreadmore/eventreadmmore.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./eventreadmore.scss";
+
 
 const EventReadMore = () => {
   const [data, setData] = useState([]);
@@ -31,11 +34,34 @@ const EventReadMore = () => {
     <div className="readCards">
       {data &&(  
         <figure className="readCard" key={data.id}>
-          <img src={`http://localhost:4000/Assets/Images/events/small/${data.image}`} alt="img" />
-          <p>{formatDate(data.startdate)} -{formatDate(data.stopdate)}</p>
+          <img src={`http://localhost:4000/Assets/Images/events/small/${data.image}`} alt="img" className="img1"/>
+          <p className="readmoredate">{formatDate(data.startdate)} -{formatDate(data.stopdate)}</p>
+          <p className="readmoreprice">BILLETPRIS:{data.price} DKK</p>
+          <div className="titlename">
           <h2>{data.title}</h2>
+          <p>{data.genre.name}</p>
+          </div>
+          <div className="buttonreadmore">
+          <Link to={`/forestillinger&events/${data.id}`}>
+            <button>LÆS MERE</button>
+          </Link>
+          </div>
+          <div className="desclength">
+          <p>{data.description}</p>
+          <p>Varighed ca. {data.duration_minutes}min</p>
+          </div>
+          <h2>MEDVIRKENDE</h2>
         </figure>
       )}
+               {data.actors && data.actors.map(actor =>{
+          return(
+            <figure key={actor.id} className="actorfig">
+              <img src={`http://localhost:4000/Assets/Images/actors/${actor.image}`} alt="actor"/>
+              <p>{actor.name} </p>
+            </figure>
+          
+          )
+         })}
     </div>
   );
 };
